a2eb9d31d53c5de18fd7df3eead1b23fb4b84bb0
Mahmoud El-Shahat <114501714+SirMahmoudElshahat@users.noreply.github.com>
2026-01-21 17:19:55 +0200

    basics done

---
+++ lib/horofy/presentation/screens/splash_screen.dart
@@ -0,0 +1,63 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:horofy/core/style/font_style.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+      splashIconSize: double.infinity,
+      duration: 4000,
+      splash: SizedBox.expand(
+        child: Image.asset('assets/images/splash.gif', fit: BoxFit.fill),
+      ),
+      nextScreen: NextScreen(),
+    );
+  }
+}
+
+class NextScreen extends StatelessWidget {
+  const NextScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/home.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Stack(
+          children: [
+            Positioned(
+              top: 300,
+              left: 139,
+              child: Text('تسجيل دخول', style: AppTextStyles.home),
+            ),
+            Positioned(
+              top: 370,
+              left: 170,
+              child: Text('طفل', style: AppTextStyles.home),
+            ),
+            Positioned(
+              top: 420,
+              left: 160,
+              child: Text('ولي الأمر', style: AppTextStyles.home),
+            ),
+            Positioned(
+              top: 475,
+              left: 180,
+              child: Text('زائر', style: AppTextStyles.home),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
